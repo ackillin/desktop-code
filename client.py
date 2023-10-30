6f16f7e1ccc26ba2cca29f7ace865c304d1df1c9
Jacob Desktop <toor@DESKTOP-6CRM0QD>
2023-10-30 09:54:01 -0500

    Updating everything before I start up my quantum stuffies.

--- client.py
+++ client.py
@@ -5,35 +5,35 @@ HOST = "ackillin.net"
 PORT = 45506
 USERNAME = input("Username? ")
 
-def diffie(conn,bits = 16):
-    #Send two public numbers (large prime, and a root prime)
-    #The client is considered "A" in this instance
-    str_enc = lambda a: str(a).encode()
+
+def diffie(conn, bits=16):
+    # Send two public numbers (large prime, and a root prime)
+    # The client is considered "A" in this instance
+
+    str_enc = lambda a:str(a).encode()
     prime,root = prims.get_both(bits)
     conn.send(str_enc(prime))
     conn.send(str_enc(root))
 
-    #Second Round
+    # Second Round
     a = prims.get_random()
-    #print(prime)
-    #print(root)
-    A = pow(prime,a,root)
+    A = pow(prime, a, root)
     B = conn.recv(1024).decode()
     B = int(B)
     conn.send(str_enc(A))
-    #print(A)
-    #print(B)
-    #print(a)
-    #Finalize key
-    key = pow(B,a,root)
+
+    # Finalize key
+    key = pow(B, a, root)
     return key
 
+
 def main_connection():
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
         key = diffie(s)
         print(key)
-        s.sendall(prims.encoder(USERNAME,key))
+        s.sendall(prims.encoder(USERNAME, key))
+
         while True:
             data = s.recv(1024).decode()
             print (data)
@@ -42,8 +42,9 @@ def main_connection():
                 s.sendall(msg.encode())
             else:
                 break
+
     print ("Broken off from server successfully")
+
 
 if __name__ == "__main__":
     main_connection()
-
--- prims.py
+++ prims.py
@@ -1,42 +1,45 @@
-import math,time,secrets
+import math
+import time
+import secrets
 
 
-def findPrimeFactors(nums,phi):
-    while phi %2==0:
+def findPrimeFactors(nums, phi):
+    while phi % 2 == 0:
         nums.add(2)
         phi //= 2
-    for i in range(3, int(math.sqrt(phi)),2):
-        while phi%i == 0:
-            #print(i)
+    for i in range(3, int(math.sqrt(phi)), 2):
+        while phi % i == 0:
             nums.add(i)
             phi = phi//i
     if phi > 2:
         nums.add(phi)
 
+
 def cleaned_root(prime):
     nums = set()
     phi = prime-1
-    findPrimeFactors(nums,phi)
-    for num in range(2,phi+1):
+    findPrimeFactors(nums, phi)
+    for num in range(2, phi+1):
         flag = False
         for factor in nums:
-            if pow(num,phi//factor,prime) == 1:
+            if pow(num, phi // factor, prime) == 1:
                 flag = True
                 break
-        if flag == False:
+        if flag is False:
             return num
     return -1
+
 
 def get_random():
     return secrets.randbelow(get_prime(16))
 
+
 def prim_root(prime):
     nums = set()
     final_set = []
-    for test in range(prime,3,-1):
-        #pow useage: base, exponent, modulo
-        for x in range(1,prime-1):
-            cur = pow(test,x,prime)
+    for test in range(prime, 3, -1):
+        for x in range(1, prime-1):
+            cur = pow(test, x, prime)
             if cur in nums:
                 nums.clear()
                 break
@@ -52,69 +55,83 @@ def prim_root(prime):
     print(f"Finished: {time.ctime()}")
     return secrets.choice(final_set)
 
-def miller(num,d):
-    #Rend below gets a positive int from 0 to num-4. We then add 2 to get rid of obviously non prime numbers
+
+def miller(num, d):
+    # Rend below gets a positive int from 0 to num-4. We then add 2 to get rid
+    # of obviously non prime numbers
+
     a = secrets.randbelow(num-5) + 3
-    x = pow(a,d,num)
+    x = pow(a, d, num)
     if x == 1 or x == num-1:
         return True
-    #While we haven't gone to or past the prime, keep checking to see if its probably prime
+
+    # While we haven't gone to or past the prime, keep checking to see if its
+    # probably prime
+
     while d != num-1:
         x = (x*x) % num
-        d *=2
-        #x = pow(x,2,num)
+        d *= 2
         if x == 1:
             return False
         if x == num-1:
             return True
+
     return False
 
-def is_prime(num,rounds):
-    if num<=1 or num == 4:
+
+def is_prime(num, rounds):
+    if num <= 1 or num == 4:
         return False
-    if num <=3:
+    if num <= 3:
         return True
     d = num-1
-    while d %2 == 0:
-        d //=2
+    while d % 2 == 0:
+        d //= 2
+
     for i in range(rounds):
-        if miller(num,d) == False:
+        if miller(num, d) is False:
             return False
     return True
+
 
 def get_prime(bits):
     import secrets
     get_prime.prime = secrets.randbits(bits)
-    while is_prime(get_prime.prime,rounds = 5) == False:
+    while is_prime(get_prime.prime, rounds=5) is False:
         get_prime.prime = secrets.randbits(bits)
         if get_prime.prime % 2 == 0:
             continue
-    #Checks the number against 5 rounds of the miller test
+    # Checks the number against 5 rounds of the miller test
     return get_prime.prime
 
+
 def diffie(conn):
-    #Send two public numbers (large prime, and a root prime)
+    # Send two public numbers (large prime, and a root prime)
     pass
-def encoder(msg,key):
+
+
+def encoder(msg, key):
     from cryptography.fernet import Fernet
     key = Fernet(key)
-    if isinstance(msg,bytes):
+    if isinstance(msg, bytes):
         return key.encrypt(msg)
     return key.encrypt(encode())
 
-def decoder(msg,key):
+
+def decoder(msg, key):
     from cryptography.fernet import Fernet
     key = Fernet(key)
-    if isinstance(msg,bytes):
+    if isinstance(msg, bytes):
         return key.decrypt(msg)
     return key.decrypt(msg.encode())
 
-def get_both(bits = 16):
+
+def get_both(bits=16):
     print("Start: {}".format(time.ctime()))
     primes = get_prime(bits)
     root = prim_root(primes)
     print(f"Finished: {time.ctime()}")
-    return (primes,root)
+    return (primes, root)
 
 
 if __name__ == "__main__":
@@ -123,4 +140,3 @@ if __name__ == "__main__":
     root = prim_root(primes)
     print(primes)
     print(root)
-
