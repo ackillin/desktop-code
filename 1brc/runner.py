42e8a33ae787eba4e5cf080aa9b708e3dcc6c964
Jacob Desktop <jacobthrowaway@outlook.com>
2024-05-01 21:36:39 -0500

    Working on Multiprocessing next, get a baseline first

--- 1brc/runner.py
+++ 1brc/runner.py
@@ -1,15 +1,20 @@
 from datetime import datetime
+from functools import reduce
 
 def main_runner(filename):
     file = open(filename)
     dicty = {}
-    for x in file:
+    for x in open(filename):
         bef,aft = x.strip().split(';')
+        aft = float(aft)
         if bef in dicty.keys():
             dicty[bef].append(aft)
         else:
             dicty.update({bef:[aft]})
     print(len(dicty['London']))
+    print("Max:", reduce(lambda a,b: a if a>b else b, dicty['London']) )
+    print("Min:", reduce(lambda a,b: a if a<b else b, dicty['London']) )
+    print("Avg:", sum(dicty['London'])/len(dicty['London']) )
 
 if __name__ == '__main__':
     cur_time = datetime.now()
@@ -18,7 +23,7 @@ if __name__ == '__main__':
     finish_time = datetime.now()
     print("Finished", finish_time)
     diff_time = finish_time - cur_time
-    print(diff_time)
-    open('times.txt','a').write(f"{finish_time.date()}, {str(diff_time.time())[6:]}\n")
+    print(str(diff_time)[5:])
+    open('times.txt','a').write(f"{finish_time.date()}, {str(diff_time)[5:]}\n")
 
     
