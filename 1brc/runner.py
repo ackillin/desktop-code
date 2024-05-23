d8ebb0175edb09a081a787134fa3dcd6c4cf6c45
Jacob Desktop <jacobthrowaway@outlook.com>
2024-05-23 14:47:01 -0500

    Pushing to testing for now

--- 1brc/runner.py
+++ 1brc/runner.py
@@ -1,20 +1,54 @@
 from datetime import datetime
 from functools import reduce
+import multiprocessing
+from multiprocessing import Queue
+import re
+import os
+
+
+def children(start=0, stop, filename)
+    dicty = {}
+    my_pid = os.getpid()
+    chunk_count = 0
+    with open(filename,'r') as file:
+        file.seek(start)
+        data = file.read(stop - start)
+        dicty = {}
+        for line in data:
+            bef,aft = line.strip().split(';')
+            aft = int(re.search(r'(-*\d+)',aft)+aft[-1])
+            if bef in dicty.keys():
+                dicty[bef].append(aft)
+            else:
+                dicty.update({bef:[aft]})
+        return dicty
+
+
 
 def main_runner(filename):
-    file = open(filename)
+    POOL_SIZE = 10
+    CHUNK_COUNT = 10000
+    TOTAL_LINES = 10_000_000
+    CHUNK_SIZE_LINES = TOTAL_LINES // CHUNK_COUNT
+    SLEEP_SECONDS = 0.1
+
+
     dicty = {}
-    for x in open(filename):
-        bef,aft = x.strip().split(';')
-        aft = float(aft)
-        if bef in dicty.keys():
-            dicty[bef].append(aft)
-        else:
-            dicty.update({bef:[aft]})
+    with multiprocessing.Manager() as manager:
+        chunk_queue: manager.Queue() = manager.Queue()
+        #Creates a pool of 10 workers
+
+        print(f"Creating pool of {POOL_SIZE} workers")
+        pool = multiprocessing.Pool(POOL_SIZE)
+        
+
+        pool.close()
+        pool.join()
     print(len(dicty['London']))
     print("Max:", reduce(lambda a,b: a if a>b else b, dicty['London']) )
     print("Min:", reduce(lambda a,b: a if a<b else b, dicty['London']) )
     print("Avg:", sum(dicty['London'])/len(dicty['London']) )
+
 
 if __name__ == '__main__':
     cur_time = datetime.now()
@@ -24,6 +58,6 @@ if __name__ == '__main__':
     print("Finished", finish_time)
     diff_time = finish_time - cur_time
     print(str(diff_time)[5:])
-    open('times.txt','a').write(f"{finish_time.date()}, {str(diff_time)[5:]}\n")
+    #open('times.txt','a').write(f"{finish_time.date()}, {str(diff_time)[5:]}\n")
 
     
