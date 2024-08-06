489e18e6207bfa0e1dcb5eb823fe85f7e267b5f2
Jacob Desktop <jacobthrowaway@outlook.com>
2024-08-05 23:09:37 -0500

    Changed over to Pandas, might try out my own optimization method later

--- 1brc/runner.py
+++ 1brc/runner.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from functools import reduce
+import pandas as pd
 
 def main_runner(filename):
     file = open(filename)
@@ -16,14 +17,24 @@ def main_runner(filename):
     print("Min:", reduce(lambda a,b: a if a<b else b, dicty['London']) )
     print("Avg:", sum(dicty['London'])/len(dicty['London']) )
 
+
+def pand_runner(filename):
+    df = pd.read_csv(filename, names = ['City','Temp'],sep=';', header=None, low_memory=True)
+    gb = df[df['City']=='London'].groupby(by='City')
+    print(gb.min(), gb.max(), gb.mean())
+
+
 if __name__ == '__main__':
     cur_time = datetime.now()
     print("Starting", cur_time)
-    main_runner('measurements.txt')
+    method = 'Pandas'
+    if method == 'First':
+        main_runner('measurements.txt')
+    elif method == 'Pandas':
+        pand_runner('measurements.txt')
     finish_time = datetime.now()
     print("Finished", finish_time)
     diff_time = finish_time - cur_time
     print(str(diff_time)[5:])
-    open('times.txt','a').write(f"{finish_time.date()}, {str(diff_time)[5:]}\n")
+    open('times.txt','a').write(f"{finish_time.date()}, {str(diff_time)[5:]}, {method}\n")
 
-    
