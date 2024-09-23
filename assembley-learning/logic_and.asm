1620c7315c61871f5408156c3c2f0a71ec286bb5
Jacob Desktop <jacobthrowaway@outlook.com>
2024-09-22 19:57:08 -0500

    forgot to add this one lol

---
+++ assembley-learning/logic_and.asm
@@ -0,0 +1,38 @@
+bits 64
+
+
+section .data
+    even_msg db 'Even Number!', 0xA, 0xD
+    len1 equ $-even_msg
+
+    odd_msg db 'Odd_Number!'
+    len2 equ $-odd_msg
+
+
+section .text
+    global _start
+
+_start:
+    mov ax, 8h
+    and ax, 1
+    jz evnn     ;Jump to even if previous was true
+
+    ; Start odd Section
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, odd_msg
+    mov edx, len2
+    int 0x80
+    jmp outprog
+
+evnn:
+    mov ah, 09h
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, even_msg
+    mov edx, len1
+    int 0x80
+
+outprog:
+    mov eax,1
+    int 0x80
