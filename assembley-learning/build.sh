f2f5223384001fb154d116c837fe6bb18b67bc42
Jacob Desktop <jacobthrowaway@outlook.com>
2024-09-26 22:56:17 -0500

    worked on taking input from user, only works for 1 octect currently

--- assembley-learning/build.sh
+++ assembley-learning/build.sh
@@ -4,3 +4,4 @@ nasm -f elf64 -o hello-linux.o -l hello-linux.lst logic_and.asm
 ld -static -o hello-linux hello-linux.o
 
 echo "Run via ./hello-linux"
+./hello-linux
--- assembley-learning/logic_and.asm
+++ assembley-learning/logic_and.asm
@@ -2,37 +2,67 @@ bits 64
 
 
 section .data
+    userMsg db 'Enter a number: '
+    lenUserMsg equ $-userMsg
+
     even_msg db 'Even Number!', 0xA, 0xD
     len1 equ $-even_msg
 
-    odd_msg db 'Odd_Number!'
+    odd_msg db 'Odd_Number!', 0xA, 0xD
     len2 equ $-odd_msg
-
+    
+section .bss
+    num resb 10
 
 section .text
     global _start
 
 _start:
-    mov ax, 8h
-    and ax, 1
-    jz evnn     ;Jump to even if previous was true
-
-    ; Start odd Section
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, odd_msg
-    mov edx, len2
+    ;Get user input and then continue with regular code
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, userMsg    ;Asks user for data
+    mov rdx, lenUserMsg
     int 0x80
-    jmp outprog
+
+    ;Read and Store input
+    mov rax, 3
+    mov rbx, 2
+    mov rcx, num    ;Reads from rcx registry (see above)
+    mov rdx, 10
+    int 0x80
+
+    
+    mov eax, [num]
+    sub eax, '0' ;Converts it to an int to compare
+    and eax, 1   ;Compares last byte to 0001, takes "and" from that
+    jz evnn     ;If it is 0 (is even) then jmp to "evnn"
+    jmp oddd    ;Jump to odd (else clause)
+    ; Start odd Section
+    ;mov eax, 4
+    ;mov ebx, 1
+    ;mov ecx, odd_msg
+    ;mov edx, len2
+    ;int 0x80
+    ;jmp outprog
 
 evnn:
-    mov ah, 09h
     mov eax, 4
     mov ebx, 1
     mov ecx, even_msg
     mov edx, len1
     int 0x80
 
-outprog:
+    mov eax, 1
+    int 0x80
+
+oddd:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, odd_msg
+    mov edx,len2
+    int 0x80
+
     mov eax,1
     int 0x80
+
